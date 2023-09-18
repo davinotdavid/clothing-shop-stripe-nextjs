ee1253f6c1b1c876df561bb872a02fcecc64978c
Davi Nakano <davinakanoca@gmail.com>
2023-09-18 09:46:36 -0700

    Add header component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import type { AppProps } from "next/app";
+import Image from "next/image";
 import { globalStyles } from "@/styles/global";
+import { Container, Header } from "@/styles/pages/app";
+import logoSVG from "@/assets/logo.svg";
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <Image src={logoSVG} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
