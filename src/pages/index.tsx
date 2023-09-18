202705b60d293e69e4b8b4f12376db1d855cb49d
Davi Nakano <davinakanoca@gmail.com>
2023-09-18 10:10:29 -0700

    Add Product component with placeholder images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,36 @@
-import { styled } from "@/styles";
-
-const Button = styled("button", {
-  backgroundColor: "$primary",
-  borderRadius: 4,
-  border: 0,
-  padding: "4px 8px",
-});
+import Image from "next/image";
+import { HomeContainer, Product } from "@/styles/pages/home";
 
 export default function Home() {
   return (
-    <>
-      <h1>Hey</h1>
-      <Button>Send</Button>
-    </>
+    <HomeContainer>
+      <Product>
+        <Image
+          src="http://placekitten.com/g/520/480"
+          alt=""
+          width={520}
+          height={480}
+        />
+
+        <footer>
+          <strong>Cat T-Shirt</strong>
+          <span>CAD 35.00</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image
+          src="http://placekitten.com/g/520/480"
+          alt=""
+          width={520}
+          height={480}
+        />
+
+        <footer>
+          <strong>Cat T-Shirt</strong>
+          <span>CAD 35.00</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   );
 }
