c01a186b926de0281d89f56236cbc8f7e4c9c25b
Davi Nakano <davinakanoca@gmail.com>
2023-09-18 14:54:39 -0700

    Styling the product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,11 +1,31 @@
+import Image from "next/image";
 import { useRouter } from "next/router";
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "@/styles/pages/product";
 
 export default function Product() {
   const { query } = useRouter();
 
   return (
-    <div>
-      <h1>Product: {JSON.stringify(query)}</h1>
-    </div>
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>T-Shirt X</h1>
+        <span>CAD 79.90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ex
+          ratione rerum repellat esse porro hic aliquid exercitationem nemo quas
+          excepturi, tempore at labore accusantium minima nam blanditiis maiores
+          iste?
+        </p>
+
+        <button>Buy now</button>
+      </ProductDetails>
+    </ProductContainer>
   );
 }
