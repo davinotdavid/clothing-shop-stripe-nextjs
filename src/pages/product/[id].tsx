a99ace8a8ca3e4dc4e806091f260f6eb6b1655ba
Davi Nakano <davinakanoca@gmail.com>
2023-09-18 15:29:34 -0700

    Fetching product data from Stripe and using SSG

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,31 +1,93 @@
-import Image from "next/image";
 import { useRouter } from "next/router";
+import Image from "next/image";
+import { GetStaticPaths, GetStaticProps } from "next";
+import Stripe from "stripe";
+import { stripe } from "@/lib/stripe";
 import {
   ImageContainer,
   ProductContainer,
   ProductDetails,
 } from "@/styles/pages/product";
 
-export default function Product() {
-  const { query } = useRouter();
+interface ProductProps {
+  product: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+    description: string;
+  };
+}
+
+export default function Product({ product }: ProductProps) {
+  const { isFallback } = useRouter();
+
+  if (isFallback) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <ProductContainer>
-      <ImageContainer></ImageContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} width={520} height={480} alt="" />
+      </ImageContainer>
 
       <ProductDetails>
-        <h1>T-Shirt X</h1>
-        <span>CAD 79.90</span>
+        <h1>{product.name}</h1>
+        <span>CAD {product.price}</span>
 
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ex
-          ratione rerum repellat esse porro hic aliquid exercitationem nemo quas
-          excepturi, tempore at labore accusantium minima nam blanditiis maiores
-          iste?
-        </p>
+        <p>{product.description}</p>
 
         <button>Buy now</button>
       </ProductDetails>
     </ProductContainer>
   );
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      { params: { id: "prod_Of5etpsszAy6up" } },
+      { params: { id: "prod_Of5eQHjmoMZC8T" } },
+      { params: { id: "prod_Of5dAjW95d2JCL" } },
+      { params: { id: "prod_Of5cAKpsMqg0v0" } },
+    ],
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params,
+}) => {
+  if (!params) {
+    return {
+      props: {},
+    };
+  }
+
+  const productId = params.id;
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ["default_price"],
+  });
+
+  const price = product.default_price as Stripe.Price;
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: price.unit_amount
+          ? new Intl.NumberFormat("en-CA", {
+              style: "currency",
+              currency: "CAD",
+            }).format(price.unit_amount / 100)
+          : null,
+        description: product.description,
+      },
+    },
+    revalidate: 60 * 60 * 1, // 1 hour
+  };
+};
