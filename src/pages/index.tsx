daacb5d97546544020714a07f28fe4699b44db3a
Davi Nakano <davinakanoca@gmail.com>
2023-09-18 10:31:46 -0700

    Add and configure keen-slider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
 import Image from "next/image";
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
 import { HomeContainer, Product } from "@/styles/pages/home";
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image
           src="http://placekitten.com/g/520/480"
           alt=""
@@ -18,7 +27,35 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
+        <Image
+          src="http://placekitten.com/g/520/480"
+          alt=""
+          width={520}
+          height={480}
+        />
+
+        <footer>
+          <strong>Cat T-Shirt</strong>
+          <span>CAD 35.00</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image
+          src="http://placekitten.com/g/520/480"
+          alt=""
+          width={520}
+          height={480}
+        />
+
+        <footer>
+          <strong>Cat T-Shirt</strong>
+          <span>CAD 35.00</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
         <Image
           src="http://placekitten.com/g/520/480"
           alt=""
